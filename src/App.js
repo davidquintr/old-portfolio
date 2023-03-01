831e1ea72e212c703d0412a328f32507f91fbc0d
davidquintr <davidquintr97@gmail.com>
2023-03-01 01:59:12 -0600

    begin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import HeaderPers from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderPers></HeaderPers>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header>
+            <img
+            src="../assets/miniicon.png"
+            ></img>
+            <h2>Hola</h2>
+        </header>
+    )
+}
+
+export default Header;
